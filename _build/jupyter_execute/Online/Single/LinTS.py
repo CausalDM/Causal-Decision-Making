21378bb95362c5ec64febc8d19f35a0a4e675258
linlinlin97 <75768141+linlinlin97@users.noreply.github.com>
2022-05-05 07:48:42 -0400

    add notebooks for the single MAB algos

---
+++ _build/jupyter_execute/Online/Single/LinTS.py
@@ -0,0 +1,120 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # LinTS
+# 
+# ## Main Idea
+# 
+# ## Algorithms Details
+# Supposed there are $K$ options, and the action space is $\mathcal{A} = \{0,1,\cdots, K-1\}$. Noticing that feature information are commonly avialable, the LinTS algorithm consdiers modeling the mean reward with items' features. As an example, considering the Gaussian reward, we assume that 
+# \begin{align}
+# \theta_{i} = \boldsymbol{x}_i^T \boldsymbol{\gamma}.
+# \end{align}
+# As for the Bernoulli reward, we assume that 
+# \begin{align}
+# \theta_{i} = logistic(\boldsymbol{x}_i^T \boldsymbol{\gamma})
+# \end{align}, where $logistic(x) \equiv 1 / (1 + exp^{-1}(x))$.
+# Similar as the standard TS algorithm, the LinTS algorithm starts with specifying a prior distribution of the parameter $\boldsymbol{\gamma}$, and a variance of the reward, based on the domian knowledge. At each round $t$, the agent will samples a vector of $\tilde{\boldsymbol{\gamma}}^{t}$ from thecorresponding posterior distribution, and the mean reward $\tilde{\boldsymbol{\theta}}^{t}$ is then calculated accordingly. The action $a$ with the greatest $\tilde{\theta}_{a}^{t}$ is then selected. Finally, the posterior distribution would be updated after receiving the feedback at the end of each round. Note that the posterior updating step differs for different pairs of prior distribution of the mean reward and reward distribution. Note that code can be easily modified to different specifications of the prior/reward distribution.
+# 
+# ## Key Steps
+# 
+# 1. Specifying a prior distirbution of $\boldsymbol{\gamma}$, and the variance of the reward distribution.
+# 2. For t = $0, 1,\cdots, T$:
+#     - sample a $\tilde{\boldsymbol{\gamma}}^{t}$ from the posterior distribution of $\boldsymbol{\gamma}$ or the prior distribution of it if in round $0$
+#     - calculated the $\tilde{\boldsymbol{\theta}}^{t}$ based on the assumed linear relationship
+#     - select action $A_t$ which has the greatest $\tilde{\theta}_{a}$, i.e. $A_t = argmax_{a \in \mathcal{A}} \tilde{\theta}_{a}^{t}$
+#     - receive the rewad $R$, and update the posterior distirbution of $\boldsymbol{\gamma}$ accordingly.
+
+# ## Demo Code
+
+# In[1]:
+
+
+# After we publish the pack age, we can directly import it
+# TODO: explore more efficient way
+# we can hide this cell later
+import os
+os.getcwd()
+os.chdir('/nas/longleaf/home/lge/CausalDM')
+# code used to import the learner
+
+
+# In[2]:
+
+
+from causaldm.learners.Online.Single import LinTS
+from causaldm.learners.Online.Single import Env
+import numpy as np
+
+
+# In[3]:
+
+
+T = 2000
+K = 5
+with_intercept = True
+p=3
+X_mu = np.zeros(p-1)
+X_sigma = np.identity(p-1)
+Sigma_theta = sigma_gamma = np.identity(p)
+mu_theta = np.zeros(p)
+seed = 0
+sigma = 1
+
+env = Env.Single_Gaussian_Env(T, K, p, sigma
+                         , mu_theta, Sigma_theta
+                        , seed = 42, with_intercept = True
+                         , X_mu = X_mu, X_Sigma = X_sigma)
+LinTS_Gaussian_agent = LinTS.LinTS_Gaussian(sigma = 1
+                                         , prior_theta_u = np.zeros(p), prior_theta_cov = np.identity(p)
+                                         , K = K, p = p)
+A = LinTS_Gaussian_agent.take_action(env.Phi)
+t = 0
+R = env.get_reward(t,A)
+LinTS_Gaussian_agent.receive_reward(t,A,R, env.Phi)
+
+
+# In[4]:
+
+
+LinTS_Gaussian_agent.cnts
+
+
+# In[5]:
+
+
+T = 2000
+K = 5
+with_intercept = True
+p=3
+X_mu = np.zeros(p-1)
+X_sigma = np.identity(p-1)
+Sigma_theta = sigma_gamma = np.identity(p)
+mu_theta = np.zeros(p)
+seed = 0
+phi_beta = 1/4
+
+env = Env.Single_Bernoulli_Env(T, K, p, phi_beta
+                         , mu_theta, Sigma_theta
+                        , seed = 42, with_intercept = True
+                         , X_mu = X_mu, X_Sigma = X_sigma)
+LinTS_Bernoulli_agent = LinTS.LinTS_Bernoulli(K = K, p = p , alpha = 1, retrain_freq = 1)
+A = LinTS_Bernoulli_agent.take_action(env.Phi)
+t = 0
+R = env.get_reward(t,A)
+LinTS_Bernoulli_agent.receive_reward(t,A,R, env.Phi)
+
+
+# In[6]:
+
+
+LinTS_Bernoulli_agent.cnts
+
+
+# **Interpretation:** A sentence to include the analysis result: the estimated optimal regime is...
+
+# ## References
+# 
+# [1] Agrawal, S., & Goyal, N. (2013, May). Thompson sampling for contextual bandits with linear payoffs. In International conference on machine learning (pp. 127-135). PMLR.
+# 
+# [2] Kveton, B., Zaheer, M., Szepesvari, C., Li, L., Ghavamzadeh, M., & Boutilier, C. (2020, June). Randomized exploration in generalized linear bandits. In International Conference on Artificial Intelligence and Statistics (pp. 2066-2076). PMLR.
---
+++ _build/jupyter_execute/Online/Single/UCB1.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # UCB1
+# 
+# ## Main Idea
+# 
+# ## Algorithms Details
+# Supposed there are $K$ options, and the action space is $\mathcal{A} = \{0,1,\cdots, K-1\}$. The UCB1 algorithm start with initializing the estimated upper confidence bound $U_a^{0}$ and the count of being pulled $C_a^{0}$ for each action $a$ as 0. At each round $t$, we greedily select an action $A_t$ as 
+# \begin{align}
+# A_t = arg max_{a\in \mathcal{A}} U_{a}^{t}.
+# \end{align}
+# 
+# After observing the rewards corresponding to the selected action $A_t$, we first update the total number of being pulled for $A_t$ accordingly. Then, we estimate the upper confidence bound for each action $a$ as
+# \begin{align}
+# U_{a}^{t+1} = \frac{1}{C_a^{t+1}}\sum_{t'=0}^{t}R_{t'}I(A_{t'}=a) + \sqrt{\frac{2*log(t+1)}{C_a^{t+1}}} 
+# \end{align}, where $R_{t'}$ is the reward received at round $t'$. Intuitively, $U_{a}^{t}$ is the sum of the sample average reward of action $a$ for expolitation and a confidence radius for exploration.
+# 
+# ## Key Steps
+# 
+# 1. Initializing the $\boldsymbol{U}^0$ and $\boldsymbol{C}^0$ for $K$ items as 0
+# 2. For t = $0, 1,\cdots, T$:
+# 
+#     2.1. select action $A_t$ as the arm with the maximum $U_a^t$
+#     
+#     2.2. Received the reward R, and update $C$ and $U$ with
+#     \begin{align}
+#     C_{A_{t}}^{t+1} &= C_{A_{t}}^{t} + 1 \\
+#     U_{A_{t}}^{t+1} &= \frac{1}{C_a^{t+1}}\sum_{t'=0}^{t}R_{t'}I(A_{t'}=a) + \sqrt{\frac{2*log(t+1)}{C_a^{t+1}}} 
+#     \end{align}
+
+# ## Demo Code
+
+# In[1]:
+
+
+# After we publish the pack age, we can directly import it
+# TODO: explore more efficient way
+# we can hide this cell later
+import os
+os.getcwd()
+os.chdir('/nas/longleaf/home/lge/CausalDM')
+# code used to import the learner
+
+
+# In[2]:
+
+
+from causaldm.learners.Online.Single import UCB1
+from causaldm.learners.Online.Single import Env
+import numpy as np
+
+
+# In[4]:
+
+
+T = 2000
+K = 5
+with_intercept = True
+p=3
+X_mu = np.zeros(p-1)
+X_sigma = np.identity(p-1)
+Sigma_theta = sigma_gamma = np.identity(p)
+mu_theta = np.zeros(p)
+seed = 0
+sigma = 1
+
+env = Env.Single_Gaussian_Env(T, K, p, sigma
+                         , mu_theta, Sigma_theta
+                        , seed = 42, with_intercept = True
+                         , X_mu = X_mu, X_Sigma = X_sigma)
+UCB_agent = UCB1.UCB1(K)
+A = UCB_agent.take_action()
+t = 0
+R = env.get_reward(t,A)
+UCB_agent.receive_reward(t,A,R)
+
+
+# In[5]:
+
+
+UCB_agent.Rs
+
+
+# **Interpretation:** A sentence to include the analysis result: the estimated optimal regime is...
+
+# ## References
+# 
+# [1] Russo, D. J., Van Roy, B., Kazerouni, A., Osband, I., & Wen, Z. (2018). A tutorial on thompson sampling. Foundations and Trends® in Machine Learning, 11(1), 1-96.
+
+# In[ ]:
+
+
+
+
