68dd4f01ef5c6b695628eba82cb5d41cb3c663f5
linlinlin97 <75768141+linlinlin97@users.noreply.github.com>
2022-11-26 19:45:17 -0500

    update online evaluation

--- _build/jupyter_execute/Online/Contextual_Bandits/Contextual_Bandits.py
+++ _build/jupyter_execute/Online/Contextual_Bandits/Contextual_Bandits.py
@@ -3,52 +3,16 @@
 
 # # Contextual Bandits
 # 
+# 
+# disjoint linear models...finite arm
+# 
+# 
 # The bandit problems have received increasing attention recently and have been widely applied to areas such as clinical trials [1], finance [2], and recommendation systems [3], among others. The most classical version of it is the multi-armed bandit (MAB) [4], where an agent will sequentially select an item (arm) from a few and then receive a random reward for the item selected. Since the reward distributions are unknown in most real applications, the central task of a MAB algorithm is to learn the distributions from feedback received and find the optimal item to maximize the cumulative rewards or, equivalently, to minimize the cumulative regret. This chapter focuses on the MAB problems by illustrating a group of classical algorithms to tackle the well-known exploration-exploitation trade-off.
 # 
 # ## Problem Setting
 # Let $T$ be the total number of rounds, and $K$ be the number of arms (actions to be selected). The agent would choose one arm at each round $t = 1, \dots, T$. Then the agent will receive the corresponding stochastic reward $R_t$ from the environment. Denote the expected reward for each arm $i$ as $r_{i}$. Since, in most real applications, such a reward distribution is always unknown, the agent needs to learn the reward distribution from feedback received. Overall, the objective is to find a bandit algorithm to maximize the cumulative Reward $\sum_{t=1}^{T}R_{t}$.
 # 
 # MAB has been extensively studied and widely applied to different areas, including healthcare, recommender system, and finance, to name a few. See [4] for a detailed review of MAB and [5] for a survey of practical applications. Among them, the ultimate goal of a learning algorithm is always to strike a good balance between exploration (try an unfamiliar action to learn more information) and exploitation (take the action that has the highest estimated reward so far) so as to maximize the cumulative reward. In the following, we will briefly illustrate three popular and classical categories of algorithms to handle the exploration-exploitation trade-off: i) $\epsilon$-greedy, ii) Upper Confidence Bound (UCB), and iii) Thompson Sampling (TS). 
-
-# ## Claasical Methods
-# ### $\epsilon$-Greedy
-# An intuitive algorithm to incorporate the exploration and exploitation is $\epsilon$-Greedy, which is simple and widely used [6]. Specifically, at each round $t$, we will select a random action with probability $\epsilon$, and select an action with the highest estimated mean reward based on the history so far with probability $1-\epsilon$. Here the parameter $\epsilon$ is pre-specified. A more adaptive variant is $\epsilon_{t}$-greedy, where the probability of taking a random action is defined as a decreasing function of $t$. Auer et al. [7] showed that $\epsilon_{t}$-greedy performs well in practice with $\epsilon_{t}$ decreases to 0 at a rate of $\frac{1}{t}$.
-# 
-# #### Supported Algorithms
-# 
-# | algorithm | Reward | with features? | Advantage |
-# |:-|:-:|:-:|:-:|
-# | [$\epsilon$-greedy]() | Binary/Gaussian | |Simple| 
-# 
-# 
-# ### Thompson Sampling
-# Thompson Sampling, also known as posterior sampling, solves the exploration-exploitation dilemma by selecting an action according to its posterior distribution [8].  At each round $t$, the agent sample the rewards from the corresponding posterior distributions and then select the action with the highest sampled reward greedily. It has been shown that, when the true reward distribution is known, a TS algorithm with the true reward distribution as the prior is nearly optimal [9]. However, such a distribution is always unknown in practice. Therefore, one of the major objectives of TS-based algorithms is to find an informative prior to guide the exploration.
-# 
-# #### Supported Algorithms
-# 
-# | algorithm | Reward | with features? | Advantage |
-# |:-|:-:|:-:|:-:|
-# | [TS [8]](https://www.ccs.neu.edu/home/vip/teach/DMcourse/5_topicmodel_summ/notes_slides/sampling/TS_Tutorial.pdf) | Binary/Guaasian | | | 
-# | [LinTS [13]](http://proceedings.mlr.press/v28/agrawal13.pdf) | Gaussian | ✅ | | 
-# | [GLMTS [12]](http://proceedings.mlr.press/v108/kveton20a/kveton20a.pdf) | GLM | ✅ | | 
-# 
-# ### Upper Confidence Bounds 
-# As the name suggested, the UCB algorithm estimates the upper confidence bound $U_{i}^{t}$ of the mean rewards based on the observations and then choose the action has the highest estimates. The class of UCB-based algorithms is firstly introduced by Auer et al. [7]. Generally, at each round $t$, $U_{i}^{t}$ is calculated as the sum of the estimated reward (exploitation) and the estimated confidence radius (exploration) of item $i$ based on $\mathcal{H}_{t}$. Then, $A_{t}$ is selected as 
-# \begin{equation}
-#     A_t = argmax_{a \in \mathcal{A}} E(R_t \mid a,\{ U_{i}^{t}\}_{i=1}^{N}).
-# \end{equation} As an example, **UCB1** [7] estimates the confidence radius as $\sqrt{\frac{2log(t)}{\text{\# item $i$ played so far}}}$. Doing so, either the item with a large average reward or the item with limited exploration will be selected.
-# 
-# #### Supported Algorithms
-# 
-# | algorithm | Reward | with features? | Advantage |
-# |:-|:-:|:-:|:-:|
-# | [UCB1 [7]](https://link.springer.com/content/pdf/10.1023/A:1013689704352.pdf) | Binary/Gaussian | | | 
-# | [LinUCB [10]](https://dl.acm.org/doi/pdf/10.1145/1772690.1772758?casa_token=CJjeIziLmjEAAAAA:CkRvgHQNqpy10rzcUP5kx31NWJmgSldd6zx8wZxskZYCoCc8v7EDIw3t3Gk1_6mfurqQTqRZ7fVA) | Guassian | ✅ | | 
-# | [UCB-GLM [11]](http://proceedings.mlr.press/v70/li17c/li17c.pdf) | GLM | ✅ | | 
-# 
-# 
-# 
-# 
 
 # ## Real Data
 # **1. MovieLens**
--- _build/jupyter_execute/Online/Contextual_Bandits/LinUCB.py
+++ _build/jupyter_execute/Online/Contextual_Bandits/LinUCB.py
@@ -4,14 +4,42 @@
 # # LinUCB
 # 
 # ## Overview
-# - **Advantage**: It is more scalable and efficient than **UCB1** by utilizing features.
+# - **Advantage**: It is more scalable and efficient than **UCB** by utilizing features.
 # - **Disadvantage**:  
 # - **Application Situation**: discrete action space, binary/Gaussian reward space
 # 
 # ## Main Idea
+# As the name suggested, the UCB algorithm estimates the upper confidence bound $U_{a}^{t}$ of the mean of the potential reward of arm $a$, $R_t(a)$, based on the observations and then choose the action has the highest estimates. The class of UCB-based algorithms is firstly introduced by Auer et al. [1]. Generally, at each round $t$, $U_{a}^{t}$ is calculated as the sum of the estimated reward (exploitation) and the estimated confidence radius (exploration) of item $i$ based on previous observations. Then, $A_{t}$ is selected as 
+# $$
+# \begin{equation}
+#     A_t = argmax_{a \in \mathcal{A}} U_a^t.
+# \end{equation} 
+# $$
+# As an example, **UCB** [1] estimates the confidence radius as $\sqrt{\frac{2log(t)}{\text{\# item $i$ played so far}}}$. Doing so, either the item with a large average reward or the item with limited exploration will be selected. Note that this algorithm support cases with either binary reward or continuous reward.
 # 
+# ## Algorithms Details
+# Supposed there are $K$ options, and the action space is $\mathcal{A} = \{0,1,\cdots, K-1\}$. The UCB1 algorithm start with initializing the estimated upper confidence bound $U_a^{0}$ and the count of being pulled $C_a^{0}$ for each action $a$ as 0. At each round $t$, we greedily select an action $A_t$ as 
+# \begin{align}
+# A_t = arg max_{a\in \mathcal{A}} U_{a}^{t}.
+# \end{align}
+# 
+# After observing the rewards corresponding to the selected action $A_t$, we first update the total number of being pulled for $A_t$ accordingly. Then, we estimate the upper confidence bound for each action $a$ as
+# \begin{align}
+# U_{a}^{t+1} = \frac{1}{C_a^{t+1}}\sum_{t'=0}^{t}R_{t'}I(A_{t'}=a) + \sqrt{\frac{2*log(t+1)}{C_a^{t+1}}} ,
+# \end{align}where $R_{t'}$ is the reward received at round $t'$. Intuitively, $U_{a}^{t}$ is the sum of the sample average reward of action $a$ for expolitation and a confidence radius for exploration.
 # 
 # ## Key Steps
+# 
+# 1. Initializing the $\boldsymbol{U}^0$ and $\boldsymbol{C}^0$ for $K$ items as 0
+# 2. For t = $0, 1,\cdots, T$:
+# 
+#     2.1. select action $A_t$ as the arm with the maximum $U_a^t$
+#     
+#     2.2. Received the reward R, and update $C$ and $U$ with
+#     \begin{align}
+#     C_{A_{t}}^{t+1} &= C_{A_{t}}^{t} + 1 \\
+#     U_{A_{t}}^{t+1} &= \frac{1}{C_a^{t+1}}\sum_{t'=0}^{t}R_{t'}I(A_{t'}=a) + \sqrt{\frac{2*log(t+1)}{C_a^{t+1}}} 
+#     \end{align}
 
 # ## Demo Code
 
@@ -68,6 +96,10 @@ LinUCB_Gaussian_agent.cnts
 
 # ## References
 # 
-# [1] Agrawal, S., & Goyal, N. (2013, May). Thompson sampling for contextual bandits with linear payoffs. In International conference on machine learning (pp. 127-135). PMLR.
-# 
-# [2] Kveton, B., Zaheer, M., Szepesvari, C., Li, L., Ghavamzadeh, M., & Boutilier, C. (2020, June). Randomized exploration in generalized linear bandits. In International Conference on Artificial Intelligence and Statistics (pp. 2066-2076). PMLR.
+# [1] Li, L., Chu, W., Langford, J., & Schapire, R. E. (2010, April). A contextual-bandit approach to personalized news article recommendation. In Proceedings of the 19th international conference on World wide web (pp. 661-670).
+
+# In[ ]:
+
+
+
+
